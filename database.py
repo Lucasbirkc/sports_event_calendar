97c304241593d09ff5b5eebc690262193e29d602
Lucch20 <Lucch20@student.sdu.dk>
2025-10-28 15:52:24 +0100

    Extended handling of GET requests and setup serving the index.html

--- database.py
+++ database.py
@@ -99,25 +99,26 @@ class EventHandler():
         )
         return cursor.fetchone()
 
-# Test DB setup and add_event function. Insertion confirmed in Database Client add-on
-with EventDB(DB_PATH) as db:
-    handler = EventHandler(db)
-    # handler.add_event("2025-10-24T11:20:00", "Basketball", "BC vs BD")
-    # handler.add_event("2025-10-24T11:25:00", "Handball", "HC vs HD")
-    # handler.add_event("2025-10-24T12:25:00", "Football", "FC vs FD")
+if __name__ == '__main__':
+    # Test DB setup and add_event function. Insertion confirmed in Database Client add-on
+    with EventDB(DB_PATH) as db:
+        handler = EventHandler(db)
+        # handler.add_event("2025-10-24T11:20:00", "Basketball", "BC vs BD")
+        # handler.add_event("2025-10-24T11:25:00", "Handball", "HC vs HD")
+        # handler.add_event("2025-10-24T12:25:00", "Football", "FC vs FD")
 
-    # handler.add_event("2025-10-25T11:20:00", "Hockey", "HoC vs HoD")
-    # handler.add_event("2025-11-24T11:20:00", "Golf", "Woods")
-    # handler.add_event("2025-12-24T11:20:00", "Disc Golf", "DA vs DD")
-    # handler.add_event("2026-12-02T11:20:00", "Ice Skating", "IC vs ID")
-    # handler.add_event("2027-12-3T11:20:00", "Dance Battle", "DBC vs DBD")
+        # handler.add_event("2025-10-25T11:20:00", "Hockey", "HoC vs HoD")
+        # handler.add_event("2025-11-24T11:20:00", "Golf", "Woods")
+        # handler.add_event("2025-12-24T11:20:00", "Disc Golf", "DA vs DD")
+        # handler.add_event("2026-12-02T11:20:00", "Ice Skating", "IC vs ID")
+        # handler.add_event("2027-12-3T11:20:00", "Dance Battle", "DBC vs DBD")
 
-    # print(f"Event 2: {handler.get_event(2)}")
-    # print(f"Event 3: {handler.get_event(3)}")
-    # print(f"Event 5: {handler.get_event(5)}")
-    # print(f"All events: {handler.get_events()}")
+        # print(f"Event 2: {handler.get_event(2)}")
+        # print(f"Event 3: {handler.get_event(3)}")
+        # print(f"Event 5: {handler.get_event(5)}")
+        # print(f"All events: {handler.get_events()}")
 
-    print(f"Next Event: {handler.get_next_event()}")
+        print(f"Next Event: {handler.get_next_event()}")
 
 # add_event("22/10/2025", "15:29", "handball", "A vs B", connection)
 # print("Event added")
--- server.py
+++ server.py
@@ -4,25 +4,39 @@ import json, urllib
 
 PORT = 8080
 PATH = 'localhost'
+INDEX_PATH = 'index.html'
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urllib.parse.urlparse(self.path)
-        print(f"parsed_path: {parsed_path}")
 
-        if parsed_path.path.startswith('/event/'):
+        # Serve index.html
+        if parsed_path.path == '/':
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/HTML')
+            self.end_headers()
+            with open(INDEX_PATH, 'rb') as file:
+                self.wfile.write(file.read())
+            return
+        # Get event with ID or get next event (e.g. /event/4 or /event/next )
+        elif parsed_path.path.startswith('/event/'):
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            event_id = int(parsed_path.path.split('/')[-1])
-            print(f"event_id: {event_id}")
-            retrieved_event = None
+            event_id = parsed_path.path.split('/')[-1]
             with EventDB(DB_PATH) as db:
                 handler = EventHandler(db)
-                retrieved_event = event_to_dict(handler.get_event(event_id))
+                if event_id == 'next':
+                    event = handler.get_next_event()
+                else:
+                    event = handler.get_event(int(event_id))
+
+                retrieved_event = event_to_dict(event)
             response_body = json.dumps(retrieved_event).encode('utf-8')
             self.wfile.write(response_body)
             return
+        elif parsed_path.path == '/events':
+            pass
 
     def do_POST(self):
         if self.path == '/event':
